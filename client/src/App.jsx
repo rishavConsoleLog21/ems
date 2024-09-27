c264a9fc63ff927712b812578c3849899ac365a7
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-28 02:08:32 +0530

    Add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Employee, Login, Register } from "./pages";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/employee" element={<Employee />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Employee from "./Employee";
+import Login from "./Login";
+import Register from "./Register";
+
+export { Dashboard, Employee, Login, Register };
