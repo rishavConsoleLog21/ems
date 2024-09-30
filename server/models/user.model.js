aee909f38ca166b8df1e34cf702c87baccf5e58d
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-30 20:42:48 +0530

    Add User Model

---
+++ server/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: [true, "Username must be unique"],
+      minlength: [3, "Username must be at least 3 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email must be unique"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
