238185c5c76ac3fe809e47a82aedbc0d80dad107
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-10-01 07:14:40 +0530

    Add Login Functionality & Route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
   const newUser = new User({ username, email, password });
@@ -8,6 +8,36 @@ export const register = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    next(errorHandler);
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    const validUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (!validUser) return next(errorHandler(401, "User not found!"));
+
+    const isValidPassword = await validUser.verifyPassword(password);
+
+    if (!isValidPassword) return next(errorHandler(401, "Invalid Credentials"));
+
+    const loggedInUser = await User.findById(validUser._id).select("-password");
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json(loggedInUser);
+  } catch (error) {
+    next(error);
   }
 };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -25,11 +25,11 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = await bcrypt.hashSync(this.password, 10);
+  this.password = bcrypt.hashSync(this.password, 10);
   next();
 });
 
-userSchema.methods.isValidPassword = async function (password) {
+userSchema.methods.verifyPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
