cd38b93a8059788c87c1c98cebf4a258c20406e1
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-10-07 20:30:24 +0530

    Fixed helmet.contentSecurityPolicy

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
-      fontSrc: ["'self'", "https://cdn.jsdelivr.net"],
-      imgSrc: ["'self'", "data:"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com", "https://cdn.jsdelivr.net"],
+      imgSrc: ["'self'", "https://firebasestorage.googleapis.com", "data:"],
     },
   })
 );
